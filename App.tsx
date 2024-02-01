b3f02c73360c8677a93921c1c2aa9597166ac18c
Alberto Junio Silva de Almeida <134745061+Heyjunio@users.noreply.github.com>
2024-02-01 18:49:18 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
+import { StatusBar } from "expo-status-bar";
 import Routes from "./src/routes";
+import { WalletContextProvider } from "./src/context/walletContext";
 
 export default function App() {
   return (
-    <>
+    <WalletContextProvider>
       <Routes />
-    </>
-  )
+      <StatusBar style="light" />
+    </WalletContextProvider>
+  );
 }
